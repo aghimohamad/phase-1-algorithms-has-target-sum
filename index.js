ecfaaf8d2e22b5a068f74bdc5a10af2bfe1decdf
mohamad <aghi.mohamad@gmail.com>
2022-06-05 12:06:25 +0300

    completed

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+   for(let i = 0; i < array.length; i++) {
+      for(let j = i + 1; j < array.length; j++) {
+         if(array[i] + array[j] == target) {
+            return true;
+         }
+      }
+   }
+   return false;
+  }
+
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  looping on each element of the array 
+  making another loop to check if any two element adds up to 10
+  if so return true 
 */
 
 /*
-  Add written explanation of your solution here
+  time : O(n^2)
+  space : O(1)
 */
 
 // You can run `node index.js` to view these console logs
